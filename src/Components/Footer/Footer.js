12a8743fa2c6e3fa1ecf2996c28007a3b7ce5cef
Harshvardhan Jha <harshvardhanjha4@gmail.com>
2023-04-18 02:11:12 +0530

    second

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaHeart } from 'react-icons/fa';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+        <div className="Footer">
+        <FaHeart className='icon'/> <a href="https://www.linkedin.com/in/harshvardhan-jha-b60148159/">Harshvardhan Jha</a><a href="https://github.com/harsh1kashyap">Github</a>
+        </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Components/Heading/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Heading.css'
+
+const Heading = () => {
+  return (
+    <div className='Head'>
+   <h1 className='heading'>Tic-tac-toe</h1>
+    </div>
+  )
+}
+
+export default Heading
